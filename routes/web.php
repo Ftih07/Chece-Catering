fe660d4ab79b890a426be2b4e790f2279d2826b6
Ftih07 <naufalfathi37@gmail.com>
2025-07-23 22:08:06 +0700

    Add error 503, 500, 404, 403 handler page

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/gallery', [GalleryController::class, 'index'])->name('gallery');
 
 Route::get('/menu', [MenuController::class, 'index'])->name('menu');
+
+Route::get('/test-error/{code}', function ($code) {
+    abort((int) $code);
+});
